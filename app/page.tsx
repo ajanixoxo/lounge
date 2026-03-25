f305c97d095fb70ed14594c1688dee44644efc54
Mukky002 <enitanpeters002@gmail.com>
2026-03-25 22:05:43 +0100

    homepage update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
-import { redirect } from "next/navigation"
+"use client"
+
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { AnimatePresence } from 'motion/react';
+import { Intro } from '@/components/layout/Intro';
 
 export default function RootPage() {
-  redirect("/lounge")
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/lounge');
+    }, 2500);
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <div className="relative min-h-screen bg-rixos-black">
+      <div className="grain" />
+      <AnimatePresence>
+        <Intro onComplete={() => router.push('/lounge')} />
+      </AnimatePresence>
+    </div>
+  );
 }
